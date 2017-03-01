488cccdc8ca3c43eaad5e309c59c398a7eaeedad
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-03-01 14:09:08 -0600

    Add Greek language by manicx

---
+++ language/el/simplespoiler.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Simple Spoiler Extension for phpBB.
+ * @author manicx
+ * @copyright 2017 Alfredo Ramos
+ * @license GNU GPL-3.0+
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB')) {
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang)) {
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'SPOILER'			=> 'Spoiler',
+	'SPOILER_HELPLINE'	=> 'Χρήση: [spoiler]κείμενο[/spoiler] ή [spoiler=τίτλος]κείμενο[/spoiler]',
+	'SPOILER_SHOW'		=> 'Εμφάνιση',
+	'SPOILER_HIDE'		=> 'Κρύψιμο'
+]);
