fb68da21bceb071ce9ee5c92477e6b5b48b08133
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-08-25 21:32:34 -0500

    Code refactoring
    This change will also make spoilers work if they are dynamically generated.

--- styles/all/theme/js/spoiler.js
+++ styles/all/theme/js/spoiler.js
@@ -2,41 +2,40 @@
  * Spoiler jQuery
  * https://github.com/AlfredoRamos/snippets/tree/master/javascript/spoiler
  * @author Alfredo Ramos <alfredo.ramos@yandex.com>
- * @version 0.1.1
+ * @version 0.2.0
  * @copyright (c) 2016 Alfredo Ramos
  * @license GNU GPL-2.0
  */
-
 (function($) {
 	'use strict';
-
-	$.fn.initSpoilers = function($options) {
-		// Overwrite options
-		$options = $.extend({
+	$.fn.initSpoilers = function($settings) {
+		// Overwrite settings
+		$settings = $.extend({
 			lang: {
 				show: 'Show',
 				hide: 'Hide'
 			},
-			selector: '.' + this.attr('class')
-		}, $options);
+			selector: '.spoiler'
+		}, $settings);
 
-		$.each(this, function() {
-			var $spoiler = {
-				wrapper: $(this),
-				trigger: $(this).children($options.selector + '-trigger').first(),
-				status: $(this).find($options.selector + '-status').first(),
-				body: $(this).children($options.selector + '-body').first()
+		// Event delegation
+		this.on('click', $settings.selector + '-trigger', function() {
+			// Spoiler elements relative to the object that is pointed to
+			var $elements = {
+				wrapper: $(this).parents($settings.selector).first(),
+				status: $(this).parents($settings.selector)
+					.find($settings.selector + '-status').first(),
+				body: $(this).parents($settings.selector)
+					.children($settings.selector + '-body').first()
 			};
 
-			$spoiler.trigger.on('click', function() {
-				// Toggle CSS class
-				$spoiler.wrapper.toggleClass($options.selector.replace('.', '') + '-show');
+			// Toggle CSS class
+			$elements.wrapper.toggleClass($settings.selector.replace('.','') + '-show');
 
-				// Toggle status text
-				$spoiler.status.text(
-					$spoiler.body.is(':visible') ? $options.lang.hide : $options.lang.show
-				);
-			});
+			// Toggle status text
+			$elements.status.text(
+				$elements.body.is(':visible') ? $settings.lang.hide : $settings.lang.show
+			);
 		});
 	};
 })(jQuery);
