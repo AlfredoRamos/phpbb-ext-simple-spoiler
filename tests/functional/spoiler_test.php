4daeedcd18384e7283ac64c44fee98fa018056fa
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-11-10 18:38:15 -0600

    Update functional tests

--- tests/functional/spoiler_test.php
+++ tests/functional/spoiler_test.php
@@ -82,4 +82,35 @@ class spoiler_test extends phpbb_functional_test_case
 		$this->assertGreaterThan(0, $result->count());
 		$this->assertContains($expected, $result->html());
 	}
+
+	public function test_deprecated_spoiler_title_bbcode()
+	{
+		$this->login();
+
+		$post = $this->create_topic(
+			2,
+			'Spoiler Functional Test 3',
+			'[spoiler=Spoiler title]Deprecated markup[/spoiler]'
+		);
+		$crawler = self::request('GET', sprintf(
+			'viewtopic.php?t=%d&sid=%s',
+			$post['topic_id'],
+			$this->sid
+		));
+
+		$expected =	'<div class="spoiler">'.PHP_EOL.
+						'<div class="spoiler-header spoiler-trigger">'.PHP_EOL.
+							'<span class="spoiler-title">Spoiler title</span>'.
+							'<span class="spoiler-status">Show</span>'.PHP_EOL.
+						'</div>'.PHP_EOL.
+						'<div class="spoiler-body">Deprecated markup</div>'.PHP_EOL.
+						'</div>';
+		$result = $crawler->filter(sprintf(
+			'#post_content%d .content',
+			$post['topic_id']
+		));
+
+		$this->assertGreaterThan(0, $result->count());
+		$this->assertContains($expected, $result->html());
+	}
 }
