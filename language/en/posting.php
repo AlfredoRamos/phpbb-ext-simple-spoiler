2ba2241310180c0b7ecb35f800747312c49b43a1
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-08-30 23:21:43 -0500

    Language files cleanup

--- language/en/posting.php
+++ language/en/posting.php
@@ -25,7 +25,5 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'SPOILER'			=> 'Spoiler',
-	'SPOILER_HELPLINE'	=> 'Usage: [spoiler]text[/spoiler] or [spoiler title=title]text[/spoiler]',
-	'SPOILER_SHOW'		=> 'Show',
-	'SPOILER_HIDE'		=> 'Hide'
+	'SPOILER_HELPLINE'	=> 'Usage: [spoiler]text[/spoiler] or [spoiler title=title]text[/spoiler]'
 ]);
