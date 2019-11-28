7860d53a9d526c248995924aa21608dc3cccaa2d
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-11-27 23:29:20 -0600

    Update tests

--- tests/event/listener_test.php
+++ tests/event/listener_test.php
@@ -22,7 +22,7 @@ class listener_test extends phpbb_test_case
 	/** @var \alfredoramos\simplespoiler\includes\helper */
 	protected $helper;
 
-	public function setUp()
+	public function setUp(): void
 	{
 		parent::setUp();
 
