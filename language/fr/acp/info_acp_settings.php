112058ef65f5f470e53c337273d605c6bc5108c0
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-05-14 13:19:03 -0500

    New translations info_acp_settings.php (French)

--- language/fr/acp/info_acp_settings.php
+++ language/fr/acp/info_acp_settings.php
@@ -24,6 +24,6 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'SPOILER_DEPTH_LIMIT' => 'Maximum nesting depth for spoilers',
-	'SPOILER_DEPTH_LIMIT_EXPLAIN' => 'Maximum spoiler nesting depth in a post. Set to <samp>0</samp> for unlimited depth.'
+	'SPOILER_DEPTH_LIMIT' => 'Nombre maximum d\'imbrication pour les spoilers',
+	'SPOILER_DEPTH_LIMIT_EXPLAIN' => 'Profondeur maximum d\'imbrication de spoiler dans un post. Réglez sur <samp>0</samp> pour une profondeur illimitée.'
 ]);
