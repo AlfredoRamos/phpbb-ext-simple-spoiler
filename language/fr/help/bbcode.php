10f2151766047d8d3310746cedc0d23bffeb9bf5
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-05-14 13:19:05 -0500

    New translations bbcode.php (French)

--- language/fr/help/bbcode.php
+++ language/fr/help/bbcode.php
@@ -24,14 +24,14 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'HELP_BBCODE_BLOCK_SPOILERS' => 'Generating Spoilers',
+	'HELP_BBCODE_BLOCK_SPOILERS' => 'Génération de spoilers',
 
-	'HELP_BBCODE_SPOILERS_BASIC_QUESTION'	=> 'Adding a spoiler into a post',
-	'HELP_BBCODE_SPOILERS_BASIC_ANSWER'		=> 'A basic spoiler consist in a text wrapped in <strong>[spoiler][/spoiler]</strong>. For example:<br><br><strong>[spoiler]</strong>%2$s<strong>[/spoiler]</strong><br><br>This would generate:<br>%1$s',
+	'HELP_BBCODE_SPOILERS_BASIC_QUESTION'	=> 'Ajouter un spoiler dans un post',
+	'HELP_BBCODE_SPOILERS_BASIC_ANSWER'		=> 'Un spoiler de base consiste en un texte enveloppé dans une balise <strong>[spoiler][/spoiler]</strong>. Par exemple:<br><br><strong>[spoiler]</strong>%2$s<strong>[/spoiler]</strong><br><br>génére<br>%1$s',
 
-	'HELP_BBCODE_SPOILERS_TITLE_QUESTION'	=> 'Adding a spoiler with title into a post',
-	'HELP_BBCODE_SPOILERS_TITLE_ANSWER'		=> 'A spoiler can optionally show a custom title, to do so the text need to be wrapped in <strong>[spoiler title=][/spoiler]</strong>. For example:<br><br><strong>[spoiler title=</strong>%3$s<strong>]</strong>%2$s<strong>[/spoiler]</strong><br><br>This would generate:<br>%1$s',
+	'HELP_BBCODE_SPOILERS_TITLE_QUESTION'	=> 'Ajouter un spoiler avec un titre dans un post',
+	'HELP_BBCODE_SPOILERS_TITLE_ANSWER'		=> 'Un spoiler peut éventuellement afficher un titre personnalisé, pour ce faire, le texte doit être enveloppé dans <strong>[spoiler title=][/spoiler]</strong>. Par exemple:<br><br><strong>[spoiler title=</strong>%3$s<strong>]</strong>%2$s<strong>[/spoiler]</strong><br><br>génère<br>%1$s',
 
-	'HELP_BBCODE_SPOILERS_DEMO_TITLE'	=> 'Plot summary',
-	'HELP_BBCODE_SPOILERS_DEMO_BODY'	=> 'Details about the movie narrative'
+	'HELP_BBCODE_SPOILERS_DEMO_TITLE'	=> 'Résumé de l\'intrigue',
+	'HELP_BBCODE_SPOILERS_DEMO_BODY'	=> 'Détails sur le récit du film'
 ]);
