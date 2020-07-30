1759a58ca16287ec0bc7d6a0fd821fc5e697a7f3
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-07-30 07:43:16 -0500

    New translations bbcode.php (French)

--- language/fr/help/bbcode.php
+++ language/fr/help/bbcode.php
@@ -26,11 +26,11 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, [
 	'HELP_BBCODE_BLOCK_SPOILERS' => 'Génération de spoilers',
 
-	'HELP_BBCODE_SPOILERS_BASIC_QUESTION'	=> 'Ajouter un spoiler dans un post',
-	'HELP_BBCODE_SPOILERS_BASIC_ANSWER'		=> 'Un spoiler de base consiste en un texte enveloppé dans une balise <strong>[spoiler][/spoiler]</strong>. Par exemple:<br><br><strong>[spoiler]</strong>%2$s<strong>[/spoiler]</strong><br><br>génére<br>%1$s',
+	'HELP_BBCODE_SPOILERS_BASIC_QUESTION'	=> 'Ajouter un spoiler dans un message',
+	'HELP_BBCODE_SPOILERS_BASIC_ANSWER'		=> 'Un spoiler de base consiste en un texte contenu dans une balise <strong>[spoiler][/spoiler]</strong>. Par exemple:<br><br><strong>[spoiler]</strong>%2$s<strong>[/spoiler]</strong><br><br>Cela générera<br>%1$s',
 
-	'HELP_BBCODE_SPOILERS_TITLE_QUESTION'	=> 'Ajouter un spoiler avec un titre dans un post',
-	'HELP_BBCODE_SPOILERS_TITLE_ANSWER'		=> 'Un spoiler peut éventuellement afficher un titre personnalisé, pour ce faire, le texte doit être enveloppé dans <strong>[spoiler title=][/spoiler]</strong>. Par exemple:<br><br><strong>[spoiler title=</strong>%3$s<strong>]</strong>%2$s<strong>[/spoiler]</strong><br><br>génère<br>%1$s',
+	'HELP_BBCODE_SPOILERS_TITLE_QUESTION'	=> 'Ajouter un spoiler avec un titre dans un message',
+	'HELP_BBCODE_SPOILERS_TITLE_ANSWER'		=> 'Un spoiler peut éventuellement afficher un titre personnalisé, pour ce faire, le texte doit être contenu dans <strong>[spoiler title=][/spoiler]</strong>. Par exemple:<br><br><strong>[spoiler title=</strong>%3$s<strong>]</strong>%2$s<strong>[/spoiler]</strong><br><br>Cela générera<br>%1$s',
 
 	'HELP_BBCODE_SPOILERS_DEMO_TITLE'	=> 'Résumé de l\'intrigue',
 	'HELP_BBCODE_SPOILERS_DEMO_BODY'	=> 'Détails sur le récit du film'
