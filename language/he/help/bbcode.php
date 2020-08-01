0f0ce7c7b556339175368d44af0ac1f32d47181e
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-08-01 11:17:22 -0500

    New translations bbcode.php (Hebrew)

--- language/he/help/bbcode.php
+++ language/he/help/bbcode.php
@@ -24,12 +24,12 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'HELP_BBCODE_BLOCK_SPOILERS' => 'Generating Spoilers',
+	'HELP_BBCODE_BLOCK_SPOILERS' => 'מייצר ספויילרים',
 
-	'HELP_BBCODE_SPOILERS_BASIC_QUESTION'	=> 'Adding a spoiler into a post',
-	'HELP_BBCODE_SPOILERS_BASIC_ANSWER'		=> 'A basic spoiler consist in a text wrapped in <strong>[spoiler][/spoiler]</strong>. For example:<br><br><strong>[spoiler]</strong>%2$s<strong>[/spoiler]</strong><br><br>This would generate:<br>%1$s',
+	'HELP_BBCODE_SPOILERS_BASIC_QUESTION'	=> 'הוסף ספויילר להודעה',
+	'HELP_BBCODE_SPOILERS_BASIC_ANSWER'		=> 'ספויילר בסיסי מוקף בתגים <strong>[spoiler][/spoiler]</strong>. לדוגמה:<br><br><strong>[spoiler]</strong>%2$s<strong>[/spoiler]</strong><br><br> מייצר:<br>%1$s',
 
-	'HELP_BBCODE_SPOILERS_TITLE_QUESTION'	=> 'Adding a spoiler with title into a post',
+	'HELP_BBCODE_SPOILERS_TITLE_QUESTION'	=> 'הוספת ספויילר עם כותרת לתוך ההודעה',
 	'HELP_BBCODE_SPOILERS_TITLE_ANSWER'		=> 'A spoiler can optionally show a custom title, to do so the text need to be wrapped in <strong>[spoiler title=][/spoiler]</strong>. For example:<br><br><strong>[spoiler title=</strong>%3$s<strong>]</strong>%2$s<strong>[/spoiler]</strong><br><br>This would generate:<br>%1$s',
 
 	'HELP_BBCODE_SPOILERS_DEMO_TITLE'	=> 'Plot summary',
