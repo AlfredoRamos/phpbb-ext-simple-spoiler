0d28626918bbe17484d02f6a304852d99031ec2d
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-08-01 11:28:42 -0500

    New translations posting.php (Hebrew)

--- language/he/posting.php
+++ language/he/posting.php
@@ -24,6 +24,6 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'SPOILER'			=> 'Spoiler',
-	'SPOILER_HELPLINE'	=> 'Usage: [spoiler]text[/spoiler] or [spoiler title=title]text[/spoiler]'
+	'SPOILER'			=> 'ספויילר',
+	'SPOILER_HELPLINE'	=> 'שימוש: [spoiler]טקסט[/spoiler] או [spoiler title=title]טקסט[/spoiler]'
 ]);
