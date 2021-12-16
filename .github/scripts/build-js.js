18560636d5e28b16de2e1f3f1b95259244465ba6
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-12-15 19:39:45 -0600

    Rewrite build helper

---
+++ .github/scripts/build-js.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const uglifyjs = require('uglify-js');
+const helper = require('./helper');
+
+const jsFileList = glob.sync(helper.buildPath + '/styles/**/theme/js/*.js').concat(glob.sync(helper.buildPath + '/adm/style/js/*.js'));
+
+jsFileList.forEach(function(j) {
+	if (j.endsWith('.min.js')) {
+		return;
+	}
+
+	const minFileName = j.replace('.js', '.min.js');
+	const isMinified = fs.existsSync(minFileName);
+
+	if (isMinified){
+		return;
+	}
+
+	const js = fs.readFileSync(j).toString();
+	const result = uglifyjs.minify(js, {
+		toplevel: true,
+		output: {
+			quote_style: 1,
+			shebang: false
+		},
+		mangle: {
+			toplevel: true
+		}
+	});
+
+	fs.writeFileSync(minFileName, result.code, {mode: 0o644});
+});
